7274b90139f600d17b7659779ca28059a0645dfb
Nesarul <nesarul@yahoo.com>
2022-12-07 09:08:00 +0600

    captcha removed

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-		<script src="https://www.google.com/recaptcha/api.js?render=6LcIu0gjAAAAAKvA4UxSEWNtWl_U5nWHhj9Y_1By"></script>
 		
 	</script>
 	</head>
@@ -45,14 +44,5 @@
 			</div>
 			</div>
 		</div>
-		<script>
-		
-			grecaptcha.ready(function() {
-				// grecaptcha.execute('6LcIu0gjAAAAAKvA4UxSEWNtWl_U5nWHhj9Y_1By', {action: 'submit'}).then(function(token) {
-				grecaptcha.execute('6LcIu0gjAAAAAKvA4UxSEWNtWl_U5nWHhj9Y_1By', {action: 'homepage'}).then(function(token) {
-					docuemnt.getElementById('secToken').value = token;
-				});
-			});
-		
 	</body>
 	</html>
